5d11bf68e42ede994cae8e343988982207799e51
slice of code <slice_of_code@mail.ru>
2024-03-04 12:57:52 +0400

    ternary

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,17 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 const tasks1 = [
-    { id: 1, title: 'HTML&CSS', isDone: true },
-    { id: 2, title: 'JS', isDone: true },
-    { id: 3, title: 'ReactJS', isDone: false },
+    {id: 1, title: 'HTML&CSS', isDone: true},
+    {id: 2, title: 'JS', isDone: true},
+    {id: 3, title: 'ReactJS', isDone: false}
 ]
 
-const tasks2 = [
-    { id: 1, title: 'Hello world', isDone: true },
-    { id: 2, title: 'I am Happy', isDone: false },
-    { id: 3, title: 'Yo', isDone: false },
-]
+const tasks2: any[] = []
 
 function App() {
     return (
         <div className="App">
-            <Todolist title={'First Todolist'} tasks={tasks1} date={'03.03.24'}/>
+            <Todolist title={'First Todolist'} tasks={tasks1} />
             <Todolist title={'Second Todolist'} tasks={tasks2}/>
         </div>
     );
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -3,7 +3,6 @@ import React from "react";
 type TodolistType = {
     title: string
     tasks: TaskType[]
-    date?: string
 }
 
 type TaskType = {
@@ -12,25 +11,27 @@ type TaskType = {
     isDone: boolean
 }
 
-export const Todolist = (props: TodolistType) => {
+export const Todolist = ({title, tasks}: TodolistType) => {
     return <div>
-        <h3>{props.title}</h3>
+        <h3>{title}</h3>
         <div>
             <input/>
             <button>+</button>
         </div>
-        <ul>
-            {props.tasks.map(task => {
-                return <li key={task.id}><input type="checkbox" checked={task.isDone}/> <span>{task.title}</span></li>
-            })}
-        </ul>
+        {tasks.length === 0 ? (<p>Тасок нет!</p>) : (
+            <ul>
+                {tasks.map(task => {
+                    return <li key={task.id}>
+                        <input type="checkbox" checked={task.isDone}/>
+                        <span>{task.title}</span>
+                    </li>
+                })}
+            </ul>
+        )}
         <div>
             <button>All</button>
             <button>Active</button>
             <button>Completed</button>
-        </div>
-        <div>
-            {props.date}
         </div>
     </div>
 }
